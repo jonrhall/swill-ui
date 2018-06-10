8736dcef1ffa397510da11751e8d2f3c0e7b37ef
jonrhall <jonrhall@users.noreply.github.com>
2018-06-10 00:22:07 -0700

    Added react-router-dom to project

--- src/core/App.jsx
+++ src/core/App.jsx
@@ -1,11 +1,17 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import AppView from '../components/AppView';
 
 const App = () => (
   <React.Fragment>
     <CssBaseline />
-    <AppView />
+    <Router>
+      <div>
+        <Route exact path="/" render={() => <Link to="/Actors">Link to Actors</Link>} />
+        <Route path="/Actors" component={AppView} />
+      </div>
+    </Router>
   </React.Fragment>
 );
 
--- webpack.config.js
+++ webpack.config.js
@@ -7,6 +7,9 @@ module.exports = {
   resolve: {
     extensions: ['.js', '.jsx']
   },
+  devServer: {
+    historyApiFallback: true
+  },
   module: {
     rules: [
       {
